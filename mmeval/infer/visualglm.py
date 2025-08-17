da8fccdd3a18a576e2e92ea6e4e01f20af07f671
yuexuanliu0802@gmail.com <yuexuanliu0802@gmail.com>
2025-08-17 20:47:12 +0000

    add visualglm locally

--- mmeval/infer/visualglm.py
+++ mmeval/infer/visualglm.py
@@ -1,10 +1,13 @@
 import re
 import copy
 import torch
-from transformers import AutoModel, AutoTokenizer
+import os
+from transformers import AutoTokenizer, AutoConfig
 from mmeval.infer.task import Task
 from mmeval.utils import constants
 from mmeval.utils.argparser import parse_args
+
+from visualglm_6b.modeling_chatglm import ChatGLMForConditionalGenerationWithImage
 
 class TaskRunner(Task):
     def __init__(self, args):
@@ -13,18 +16,25 @@ class TaskRunner(Task):
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     def load_model(self, args):
-        model_path = f"THUDM/{args.model_name_or_path}"
-        self.tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
-        self.model = AutoModel.from_pretrained(model_path, trust_remote_code=True).half().cuda()
+        model_dir = os.path.abspath("./mmeval/infer/visualglm_6b")
+        weight_dir = os.path.abspath("/models/visualglm_6b")
+        self.tokenizer = AutoTokenizer.from_pretrained(model_dir, trust_remote_code=True, local_files_only=True)
+        self.config = AutoConfig.from_pretrained(model_dir, trust_remote_code=True, local_files_only=True)
+        self.model = ChatGLMForConditionalGenerationWithImage.from_pretrained(
+            weight_dir,
+            local_files_only=True,
+            config=self.config,
+            torch_dtype=torch.float16,
+            device_map="auto").half().cuda()
 
     def run_sample(self, sample: dict):
         ori_sample = copy.deepcopy(sample)
         question, modality = self.parse_input(sample)
 
         if not self.args.score_target:
-            ori_sample["response"] = self._generate_response(question, sample["media_dir"], None)
+            ori_sample["response"] = self._generate_response(question, sample["media"], None)
         else:
-            ori_sample.update(self._score_choices(question, sample["media_dir"], None, sample))
+            ori_sample.update(self._score_choices(question, sample["media"], None, sample))
 
         return ori_sample
 
