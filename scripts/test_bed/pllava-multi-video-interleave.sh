b4546054a8815502428b487fabdeebd1e89ff75b
ZTWHHH <ztw371@gmail.com>
2025-08-26 07:09:19 +0000

    fix: change the script

--- scripts/test_bed/pllava-multi-video-interleave.sh
+++ scripts/test_bed/pllava-multi-video-interleave.sh
@@ -11,22 +11,22 @@ python mmeval/run.py \
     --parallel_per_task 1 \
     --max_new_tokens 512
 
-# python mmeval/run.py \
-#     --infile test_bed/modality_test/task/multi_image_interleave.json \
-#     --dataset local@json \
-#     --out_dir work_dirs/pllava-13b-multi-video-interleave \
-#     --img_dir test_bed/modality_test/media/448 \
-#     --model_name_or_path work_dirs/pllava-7b \
-#     --gpu_per_parallel 1 \
-#     --parallel_per_task 1 \
-#     --max_new_tokens 512
+python mmeval/run.py \
+    --infile test_bed/modality_test/task/multi_image_interleave.json \
+    --dataset local@json \
+    --out_dir work_dirs/pllava-13b-multi-video-interleave \
+    --img_dir test_bed/modality_test/media/448 \
+    --model_name_or_path /root/zhichengjiang/simple-mmeval/models/pllava-13b \
+    --gpu_per_parallel 1 \
+    --parallel_per_task 1 \
+    --max_new_tokens 512
 
-# python mmeval/run.py \
-#     --infile test_bed/modality_test/task/multi_image_interleave.json \
-#     --dataset local@json \
-#     --out_dir work_dirs/pllava-34b-multi-video-interleave \
-#     --img_dir test_bed/modality_test/media/448 \
-#     --model_name_or_path work_dirs/pllava-7b \
-#     --gpu_per_parallel 1 \
-#     --parallel_per_task 1 \
-#     --max_new_tokens 512
+python mmeval/run.py \
+    --infile test_bed/modality_test/task/multi_image_interleave.json \
+    --dataset local@json \
+    --out_dir work_dirs/pllava-34b-multi-video-interleave \
+    --img_dir test_bed/modality_test/media/448 \
+    --model_name_or_path /root/zhichengjiang/simple-mmeval/models/pllava-34b \
+    --gpu_per_parallel 1 \
+    --parallel_per_task 1 \
+    --max_new_tokens 512
