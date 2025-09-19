260830e22d7cac9faa2430b1fab1054444c2e92d
snorfyang <snorfyang@gmail.com>
2025-09-19 01:26:23 +0000

    update

--- scripts/test_bed/llava_next_video.sh
+++ scripts/test_bed/llava_next_video.sh
@@ -1,8 +1,11 @@
 export PYTHONPATH=./:$PYTHONPATH
 
-python mmeval/infer/llava_next_video.py \
-    --infile test_bed/image.json \
+python mmeval/run.py \
+    --infile test_bed/modality_test/task/single_image_start.json \
     --dataset local@json \
-    --out_dir test_bed/test_llava_ov \
-    --img_dir test_bed \
-    --model_name_or_path llava-hf/LLaVA-NeXT-Video-7B-hf
+    --out_dir work_dirs/llava_next_video-single-image-start \
+    --img_dir test_bed/modality_test/media/448 \
+    --model_name_or_path llava-hf/LLaVA-NeXT-Video-7B-hf \
+    --gpu_per_parallel 1 \
+    --parallel_per_task 1 \
+    --max_new_tokens 512
