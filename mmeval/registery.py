b8d5c8d6670699b5a29da4b3a3d10e96ea5bed16
Irisicy4 <61894955+Irisicy4@users.noreply.github.com>
2025-10-03 12:51:39 -0400

    Update registery.py

--- mmeval/registery.py
+++ mmeval/registery.py
@@ -9,7 +9,7 @@ series_mapping = {
                          "LLaVA-NeXT-Video-7B",
                          "LLaVA-NeXT-Video-7B_multi_frame",
                          "LLaVA-NeXT-Video-7B-DPO",
-                         "LLaVA-NeXT-Video-7B-DPO_multi_frame"]
+                         "LLaVA-NeXT-Video-7B-DPO_multi_frame"],
     # "blip2_flan_t5": ["blip2-flan-t5-xl", "blip2-flan-t5-xxl"],
     "cambrian": ["cambrian-8b", "cambrian-13b", "cambrian-34b", "cambrian-phi3-3b"],
     "gemma3": ["gemma-3-4b-it", "gemma-3-12b-it", "gemma-3-27b-it"],
